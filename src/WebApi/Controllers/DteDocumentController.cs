ec21e6f29ad2919bcaa87ebe2245bada1a1c0fce
FelipeAndresBascunanMorales <f.bascunan03@gmail.com>
2025-04-06 20:51:00 -0400

    feat: an endpoint to return a fake dteDocument was added

---
+++ src/WebApi/Controllers/DteDocumentController.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities;
+using Domain.Ports;
+using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Serialization;
+
+namespace WebApi.Controllers
+{
+    public class DteDocumentController : Controller
+    {
+        private readonly IDteDocumentRepository mockSiiService;
+        public DteDocumentController(IDteDocumentRepository mockSiiService)
+        {
+            this.mockSiiService = mockSiiService;
+        }
+
+        [HttpGet("dte/{code}")]
+        public async Task<IActionResult> GetDteDocumentsAsXml(string code)
+        {
+            var dteDocument = await mockSiiService.GetByIdAsync(code);
+
+            if(dteDocument == null)
+            {
+                return NotFound(new { Error = "DTE document not found" });
+            }
+
+            var xmlSerializer = new XmlSerializer(typeof(DteDocument));
+            using (var stringWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(stringWriter, dteDocument);
+                return Content(stringWriter.ToString(), "application/xml");
+            }
+        }
+    }
+}
